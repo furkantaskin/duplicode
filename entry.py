7ea4afb7a93fb45f82a441b37b4ae8a2c37eb5af
Furkan Taşkın <furkan.taskin@tuta.io>
2022-11-16 11:30:30 +0300

    Move process id generation from main.py to entry.py

--- entry.py
+++ entry.py
@@ -3,7 +3,8 @@ Get input from user to copy webpage
 """
 
 from playwright.sync_api import Playwright, sync_playwright
-from main import cp_run
+from duplicode import main
+import time
 
 create_folder = input("Give me the folder you wanna create: ")
 res_folder = input("Choose folder to copy: ")
@@ -12,8 +13,9 @@ video_option = input("Do you want video record (y/n): ").lower()
 check_video = True if video_option == "y" else False
 
 print("Input can't be empty") if create_folder in ["\n", " "] else print("Starting Process for cPanel")
-
+get_id = int(time.time())
+print("Current process id is", get_id)
 options = ["cPanel", "CWP", "Plesk"]
 
 with sync_playwright() as p:
-    cp_run(p, create_folder, res_folder, check_video)
+    main.cp_run(p, create_folder, res_folder, check_video, get_id)

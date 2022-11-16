762484f99802bcea7053e888198278ceacb6cd42
Furkan Taşkın <furkan.taskin@tuta.io>
2022-11-16 09:29:40 +0300

    Fix selection of buttons on file manager

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from playwright.sync_api import Playwright, expect
 from decouple import config
 import re
+import time
 
 
 def cp_run(playwright: Playwright, dest_folder, source_folder, is_video):
+    get_id = time.time()
     # Runs playwright and logins to whm panel
-    browser = playwright.chromium.launch(channel="chrome", headless=False)
+    browser = playwright.chromium.launch(channel="chrome", slow_mo=1000, headless=True)
     # Choose video directory if user wants video
-    context = browser.new_context(no_viewport=True, record_video_dir="videos/" if is_video else None)
-
+    context = browser.new_context(no_viewport=True, record_video_dir=f"videos/{int(get_id)}" if is_video else None)
+    context.tracing.start(screenshots=True, snapshots=True, sources=True)
     page = context.new_page()
 
     print("Logging into cPanel")
@@ -26,12 +28,14 @@ def cp_run(playwright: Playwright, dest_folder, source_folder, is_video):
     print("Creating destination folder")
     page1.locator("input#location").fill(f"public_html/demo")
     page1.locator("button#btnGo").click()
-    page.wait_for_timeout(500)
+    page1.wait_for_timeout(500)
     page1.get_by_role("link", name=re.compile("Klasör")).click()
     page1.locator("input#new-folder-name").fill(f"{dest_folder}")
     page1.get_by_role("button", name="Create New Folder").click()
+    page1.wait_for_timeout(600)
 
     # Create zip from source folder
+    print("Kaynak klasör sıkıştırılıyor")
     page1.locator("input#location").fill(f"public_html/demo/{source_folder}")
     page1.locator("button#btnGo").click()
     page.wait_for_timeout(500)
@@ -41,32 +45,37 @@ def cp_run(playwright: Playwright, dest_folder, source_folder, is_video):
     page1.locator("input[name='compressfilepath']").fill(f"/public_html/demo/{source_folder}/transfer.zip")
     page1.get_by_role("button", name="Compress Files").click()
     page.wait_for_timeout(5000)
-    page1.locator("button:has-text(\"Close\"").click()
-    page1.get_by_role("link", name=re.compile("Yeniden Yükle")).click()
-    page1.locator(".yui-dt-rec:has-text(\"transfer.zip\")").click()
+    page1.get_by_role("button", name="Close").click()
+    page1.get_by_role("link", name=" Yeniden yükle").click()
+    page1.get_by_role("cell", name="transfer.zip").locator("div:has-text(\"transfer.zip\")").click()
     page1.wait_for_timeout(500)
-    page1.locator(".yui-dt-rec:has-text(\"transfer.zip\")").click(button="right")
+    print("Dosyalar taşınyor")
     page1.get_by_role("link", name=re.compile("Taşı")).click()
-    page1.get_by_label("Geçerli Yol:").fill(f"/public_html/demo/{dest_folder}")
-    page1.get_by_role("button", name="Move Files").click()
+    page1.get_by_label("Geçerli Yol:").fill(f"/public_html/demo/{dest_folder}/")
+    page1.keyboard.press("Enter")
 
     # Go to the destination folder to extract the zip
-    page1.locator("input#location").fill(f"public_html/demo/{dest_folder}")
+    page1.locator("input#location").fill(f"public_html/demo/{dest_folder}/")
     page1.locator("button#btnGo").click()
     page.wait_for_timeout(500)
     page1.locator(".yui-dt-rec:has-text(\"transfer.zip\")").click()
     page1.wait_for_timeout(500)
     page1.locator(".yui-dt-rec:has-text(\"transfer.zip\")").click(button="right")
+    print("Dosya çıkarılıyor")
     page1.get_by_role("link", name=re.compile("Çıkar")).click()
     page1.get_by_role("button", name="Extract Files").click()
     page1.get_by_role("button", name="Close").click()
-    page1.get_by_role("link", name=re.compile("Yeniden Yükle")).click()
+    page1.get_by_role("link", name=" Yeniden yükle").click()
 
     # Delete the zip file
+    print("Gereksiz dosyalar siliniyor")
     page1.locator(".yui-dt-rec:has-text(\"transfer.zip\")").click()
     page1.wait_for_timeout(500)
     page1.locator(".yui-dt-rec:has-text(\"transfer.zip\")").click(button="right")
     page1.get_by_role("link", name=re.compile("Sil")).click()
     page1.get_by_label("Dosyaları çöp kutusuna atmadan kalıcı olarak silin").check()
     page1.get_by_role("button", name="Confirm").click()
-    page.pause()
+    context.tracing.stop(path=f"traces/trace_{int(get_id)}.zip")
+    print("Shutting Down")
+    context.close()
+    browser.close()
